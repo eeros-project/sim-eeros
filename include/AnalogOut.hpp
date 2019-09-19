ca2693ba81806d03615bd070cc591fc2c226684e
ursgraf <urs.graf@ntb.ch>
2019-09-19 12:19:39 +0200

    Correct get method

--- include/AnalogOut.hpp
+++ include/AnalogOut.hpp
@@ -8,23 +8,24 @@
 #include "SimChannel.hpp"
 
 namespace sim {
-	class AnalogOut : public eeros::hal::ScalableOutput<double> {
-	public:
-		AnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
-			  double scale = 1, double offset = 0, 
-			  double rangeMin = std::numeric_limits<double>::min() , double rangeMax = std::numeric_limits<double>::max(), 
-			  std::string unit = "");
-		virtual double get();
-		virtual void set(double voltage);
-		
-	private:
-		std::shared_ptr<sim::SimChannel<double>> chan;
-	};
+  
+class AnalogOut : public eeros::hal::ScalableOutput<double> {
+ public:
+  AnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
+            double scale = 1, double offset = 0, 
+            double rangeMin = std::numeric_limits<double>::min() , double rangeMax = std::numeric_limits<double>::max(), 
+            std::string unit = "");
+  virtual double get();
+  virtual void set(double voltage);
+
+ private:
+  std::shared_ptr<sim::SimChannel<double>> chan;
+};
 };
 
 extern "C"{
-	eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
-							    double scale, double offset, double rangeMin, double rangeMax, std::string unit);
+  eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
+                                                      double scale, double offset, double rangeMin, double rangeMax, std::string unit);
 }
 
 #endif /* SIM_EEROS_ANALOGOUT_HPP_ */
--- lib/AnalogOut.cpp
+++ lib/AnalogOut.cpp
@@ -3,37 +3,30 @@
 
 using namespace sim;
 
-AnalogOut::AnalogOut(std::string id,
-					 void* libHandle,
-					 std::string device,
-					 uint32_t subDeviceNumber,
-					 uint32_t channel,
-					 double scale,
-					 double offset,
-					 double rangeMin,
-					 double rangeMax,
-					 std::string unit
-		    ) : ScalableOutput<double>(id, libHandle, scale, offset, rangeMin, rangeMax, unit) {
-	SimDevice *dev = SimDevice::getDevice(device);
-	this->chan = dev->getRealChannel(subDeviceNumber, channel);
+AnalogOut::AnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber,
+                     uint32_t channel, double scale, double offset, double rangeMin, double rangeMax,
+                     std::string unit) 
+    : ScalableOutput<double>(id, libHandle, scale, offset, rangeMin, rangeMax, unit) {
+  SimDevice *dev = SimDevice::getDevice(device);
+  this->chan = dev->getRealChannel(subDeviceNumber, channel);
 }
 
 double AnalogOut::get() {
-	return (chan->getValue() - offset) / scale;
+  return chan->getValue() * scale + offset;
 }
 
 void AnalogOut::set(double voltage) {
-	double value = (voltage - offset)/scale;
-	
-	if(value > maxOut) value = maxOut;
-	if(value < minOut) value = minOut;
-	
-	chan->setValue(value);
+  double value = (voltage - offset)/scale;
+
+  if(value > maxOut) value = maxOut;
+  if(value < minOut) value = minOut;
+
+  chan->setValue(value);
 }
 
 extern "C"{
-	eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
-							    double scale, double offset, double rangeMin, double rangeMax, std::string unit){
-		return new sim::AnalogOut(id, libHandle, device, subDeviceNumber, channel, scale, offset, rangeMin, rangeMax, unit);
-	}
+  eeros::hal::ScalableOutput<double> *createAnalogOut(std::string id, void* libHandle, std::string device, uint32_t subDeviceNumber, uint32_t channel, 
+                                                      double scale, double offset, double rangeMin, double rangeMax, std::string unit) {
+    return new sim::AnalogOut(id, libHandle, device, subDeviceNumber, channel, scale, offset, rangeMin, rangeMax, unit);
+  }
 }
